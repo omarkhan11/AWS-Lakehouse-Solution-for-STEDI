c0f59cfcf82d0785da8f85dd1e438c1662edcd2a
omarkhan11 <131040449+omarkhan11@users.noreply.github.com>
2025-03-08 11:25:22 -0500

    Update customer_landing.sql

--- Landing Zone/customer/customer_landing.sql
+++ Landing Zone/customer/customer_landing.sql
@@ -1,1 +1,21 @@
-
+CREATE EXTERNAL TABLE `customer_landing`(
+  `customername` string COMMENT 'from deserializer', 
+  `email` string COMMENT 'from deserializer', 
+  `phone` string COMMENT 'from deserializer', 
+  `birthday` date COMMENT 'from deserializer', 
+  `serialnumber` string COMMENT 'from deserializer', 
+  `registrationdate` timestamp COMMENT 'from deserializer', 
+  `lastupdatedate` timestamp COMMENT 'from deserializer', 
+  `sharewithresearchasofdate` timestamp COMMENT 'from deserializer', 
+  `sharewithpublicasofdate` timestamp COMMENT 'from deserializer', 
+  `sharewithfriendsasofdate` timestamp COMMENT 'from deserializer')
+ROW FORMAT SERDE 
+  'org.openx.data.jsonserde.JsonSerDe' 
+STORED AS INPUTFORMAT 
+  'org.apache.hadoop.mapred.TextInputFormat' 
+OUTPUTFORMAT 
+  'org.apache.hadoop.hive.ql.io.HiveIgnoreKeyTextOutputFormat'
+LOCATION
+  's3://stedidb/customer/landing/'
+TBLPROPERTIES (
+  'classification'='json')
